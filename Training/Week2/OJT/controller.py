dcd2462e58aca844635eae2e559a006d04070b7f
unknown <weedkhalifa1608@gmail.com>
2026-01-21 18:00:58 +0900

    feature: created api responing data from pkl file

---
+++ Training/Week2/OJT/controller.py
@@ -0,0 +1,21 @@
+from repository import get_repo
+import json
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+path = str(BASE_DIR / "data_json.pkl")
+repo = get_repo(path)
+def convertToJson(dict) -> json:
+    return json.dumps(dict,indent=4,default=str,ensure_ascii=False)
+
+# Giả lập api
+def get_all_user_purchase() -> json:
+    return convertToJson(repo.getAllUserPurchase())
+
+def get_user_by_id(user_id) -> dict:
+    return repo.getUserById(user_id)
+
+
+if __name__ == '__main__':
+    #print(get_all_user_purchase())
+    print(get_user_by_id('m6zc646sqc8akVQSnJfecq'))
